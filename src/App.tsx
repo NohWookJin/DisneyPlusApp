9a7f9d1b2be862cfcfcfd2d5b44ebd1198b99f42
NohWookJin <jinn98@kakao.com>
2023-10-13 16:37:15 +0900

    Feat: 컴포넌트 import

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import styled from 'styled-components'
 import './App.css'
 import Nav from './components/Nav'
 import Banner from './components/Banner'
+import Category from './components/Category'
+import Row from './components/Row'
+import requests from './api/request'
 
 function App() {
 
@@ -9,6 +12,11 @@ function App() {
     <Container>
       <Nav />
       <Banner />
+      <Category />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
     </Container>
   )
 }
